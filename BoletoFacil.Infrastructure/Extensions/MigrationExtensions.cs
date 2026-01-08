0fa90585a325441e053950713953882e3a33f359
yuripeixinho <yuripeixinho03@gmail.com>
2026-01-08 19:55:15 -0300

    feat: configurar migracao automatica para ambiente de desenvolvimento

--- BoletoFacil.Infrastructure/Extensions/MigrationExtensions.cs
+++ BoletoFacil.Infrastructure/Extensions/MigrationExtensions.cs
@@ -1,20 +1,20 @@
-﻿//using BoletoFacil.Infrastructure.Data.Context;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Hosting;
+﻿using BoletoFacil.Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-//namespace BoletoFacil.Infrastructure.Extensions;
+namespace BoletoFacil.Infrastructure.Extensions;
 
-//public static class MigrationExtensions 
-//{
-//    public static void ApplyMigrations(this IHost app)
-//    {
-//        using var scope = app.Services.CreateScope();
-//        var db = scope.ServiceProvider.GetRequiredService<BoletoFacilContext>();
+public static class MigrationExtensions 
+{
+    public static void ApplyMigrations(this IHost app)
+    {
+        using var scope = app.Services.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<BoletoFacilContext>();
 
-//        if (db.Database.GetPendingMigrations().Any())
-//        {
-//            db.Database.Migrate();
-//        }
-//    }
-//}
+        if (db.Database.GetPendingMigrations().Any())
+        {
+            db.Database.Migrate();
+        }
+    }
+}
